95830d2a7aa0bdbc3216dfed8e8080418775c6f1
David Scales <davidsscales@gmail.com>
2018-01-23 15:12:22 -0800

    refactor(starter&solution): update solution for Workbox v3
    This commit updates the solution code to use Workbox v3.
    The current solution does not cover the optional codelab
    tasks - that will be added in a later commit.
    
    This commit also further updates/refactors the starter code.

--- solution/gulpfile.js
+++ solution/gulpfile.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 Google Inc.
+Copyright 2018 Google Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,50 +13,52 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// import our dependencies
 const gulp = require('gulp');
 const del = require('del');
 const runSequence = require('run-sequence');
-// uncomment the line below:
-const wbBuild = require('workbox-build');
+const workboxBuild = require('workbox-build');
 
+// this task removes old files
 gulp.task('clean', () => del(['.tmp', 'build/*', '!build/.git'], {dot: true}));
 
+// this task copies "app" files into "build"
 gulp.task('copy', () =>
   gulp.src([
     'app/**/*',
   ]).pipe(gulp.dest('build'))
 );
 
+// this is our default task
 gulp.task('default', ['clean'], cb => {
   runSequence(
     'copy',
-    // uncomment the line below:
-    'build-sw',
+    'service-worker', // adds the service worker injection to our default task
     cb
   );
 });
 
+// this task watches our "app" files & rebuilds whenever they change
 gulp.task('watch', function() {
   gulp.watch('app/**/*', ['default']);
 });
 
-gulp.task('build-sw', () => {
-  return wbBuild.injectManifest({
-    swSrc: 'app/src/sw.js',
-    swDest: 'build/service-worker.js',
+// this task injects a precache manifest into our service worker
+gulp.task('service-worker', () => {
+  return workboxBuild.injectManifest({
+    swSrc: 'app/sw.js',
+    swDest: 'build/sw.js',
     globDirectory: 'build',
-    staticFileGlobs: [
+    globPatterns: [
       'style/main.css',
       'index.html',
       'js/idb-promised.js',
       'js/main.js',
       'images/**/*.*',
       'manifest.json'
-    ],
-    templatedUrls: {
-      '/': ['index.html']
-    }
-  }).catch((err) => {
-    console.log('[ERROR] This happened: ' + err);
+    ]
+  }).catch(err => {
+    console.log('[ERROR]: ' + err);
   });
 });
