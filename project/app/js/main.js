457e0fc2b3705ecf217d96414de2cefaced2ca61
David Scales <davidsscales@gmail.com>
2018-07-25 15:47:46 -0700

    refactor(gulp): gulp and dependencies to fix vulnerabilities
    This commit updates the starter & solution code to gulp v4,
    and updates dependency versions to eliminate security issues
    present in earlier dependencies.

--- project/app/js/main.js
+++ project/app/js/main.js
@@ -13,6 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// TODO - register service worker
+
 const container = document.getElementById('container');
 const offlineMessage = document.getElementById('offline');
 const noDataMessage = document.getElementById('no-data');
